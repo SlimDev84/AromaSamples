ccd2fe3d9565a994680be98371a4b4d263adf952
Simone <s_marra84@hotmail.com>
2024-03-23 16:26:21 +0000

    First commit

---
+++ home.js
@@ -0,0 +1,30 @@
+
+let price = 20.99;
+
+
+console.log(price);
+
+showMessage(price);
+
+showMessage(typeof price); //shows the variable type in this case number
+
+
+let message = "Hello";
+
+message = message.toLowerCase();
+
+showMessage(message);
+
+
+let amount = 12.3;
+
+amount = amount.toString();
+
+showMessage(amount);
+
+
+let amount2 = "33.3"
+
+amount2 = Number.parseFloat(amount2);
+
+showMessage(typeof amount2);
